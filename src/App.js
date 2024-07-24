7c777604dde6a76504fe2c2370032e5a1704ea09
Dominic Brian <dominic91960@gmail.com>
2024-07-24 16:56:16 +0530

    Fixed issues in Carousel and How to Buy sections.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Carousel from "./components/Carousel/Carousel";
-import HowToBuy from "./components/HowToBuy";
+import HowToBuy from "./components/HowToBuy/HowToBuy";
 
 function App() {
   return (
-    <div className="App bg-gradient-to-b from-[#6A2B1F] to-[#2F0C06]">
+    <div className="App">
       <Carousel />
       <HowToBuy />
     </div>
