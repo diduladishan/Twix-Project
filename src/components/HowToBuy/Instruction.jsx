cbfc78424091dd5de95a2bd43adaa910400fbfc6
Dominic Brian <dominic91960@gmail.com>
2024-07-24 20:43:06 +0530

    Style changes in How to Buy section.

--- src/components/HowToBuy/Instruction.jsx
+++ src/components/HowToBuy/Instruction.jsx
@@ -12,15 +12,17 @@ const Instruction = ({ number, title, content }) => {
 
   return (
     <div
-      className="font-montserrat uppercase mt-5 md:mt-0 group cursor-default"
+      className="font-montserrat uppercase mt-5 md:mt-0 cursor-default"
       data-aos="fade-left"
       data-aos-anchor-placement="bottom-bottom"
       data-aos-delay="200"
     >
       <p className="text-[#FE8C00] font-bold text-xs animate-pulse">{number}</p>
-      <h2 className="font-bold text-2xl lg:text-3xl group-hover:text-3xl lg:group-hover:text-4xl transition-all duration-500 h-9">
-        {title}
-      </h2>
+      <div className="flex justify-center md:justify-end">
+        <h2 className="font-bold text-2xl lg:text-3xl hover:text-3xl lg:hover:text-4xl transition-all duration-500 h-9 w-fit">
+          {title}
+        </h2>
+      </div>
       <p className="font-medium text-xs lg:text-sm">{content}</p>
     </div>
   );
