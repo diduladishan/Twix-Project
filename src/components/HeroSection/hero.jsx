a952b80368db3668b57f025ffed5564f9f13cb52
diduladishan <diduladishan14@gmail.com>
2024-07-24 21:02:57 +0530

    add hero section

---
+++ src/components/HeroSection/hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import twixLogo from "../../assets/images/heroSection/twix-logo.png";
+import chocoBarBottom from "../../assets/images/heroSection/chocolateBar-bottom.png";
+import chocoCharacterRight from "../../assets/images/heroSection/choco-character-right.png";
+import chocoCharacterLeft from "../../assets/images/heroSection/choco-character-left.png";
+import "./hero.css";
+
+function Hero() {
+  return (
+    <div className="photoDiv">
+      {/* <div className="characterContainer">
+        <img src={chocoCharacterLeft} alt="Choco" className="chocoCharacter" />
+      </div>
+      <div className="characterContainer">
+        <img src={chocoCharacterRight} alt="Choco" className="chocoCharacter" />
+      </div> */}
+      <img src={chocoBarBottom} alt="Choco" className="chocoBarBottom" />
+    </div>
+  );
+}
+
+export default Hero;
