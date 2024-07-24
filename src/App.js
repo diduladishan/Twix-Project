d7395ddf71cdd9b92fb2d27338d836d856dd951d
diduladishan <diduladishan14@gmail.com>
2024-07-24 19:54:04 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Carousel from "./components/Carousel/Carousel";
-import HowToBuy from "./components/HowToBuy";
-import Navbar from "./components/Navbar/navbar";
+import HowToBuy from "./components/HowToBuy/HowToBuy";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Carousel />
       <HowToBuy />
       <Footer />
