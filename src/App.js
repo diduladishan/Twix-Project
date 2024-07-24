4dd3309e72e861c04ad0206dcdb5ff8e00b40801
diduladishan <diduladishan14@gmail.com>
2024-07-24 17:19:58 +0530

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Carousel from "./components/Carousel/Carousel";
 import HowToBuy from "./components/HowToBuy";
+import Navbar from "./components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App bg-gradient-to-b from-[#6A2B1F] to-[#2F0C06]">
-      <Carousel />
-      <HowToBuy />
+      <Navbar />
+      {/* <Carousel />
+      <HowToBuy /> */}
     </div>
   );
 }
