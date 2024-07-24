31a8abce8e7dc60b550a7bc6787cdffac7d0fd81
diduladishan <diduladishan14@gmail.com>
2024-07-24 19:52:08 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Carousel from "./components/Carousel/Carousel";
 import HowToBuy from "./components/HowToBuy";
 import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Carousel />
       <HowToBuy />
+      <Footer />
     </div>
   );
 }
