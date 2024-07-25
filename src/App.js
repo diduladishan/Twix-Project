de1af384ab18a5c85119a18c045a35e45951a909
diduladishan <diduladishan14@gmail.com>
2024-07-25 15:05:52 +0530

    add section 03

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import HowToBuy from "./components/HowToBuy/HowToBuy";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/navbar";
 import Hero from "./components/HeroSection/hero";
+import Section03 from "./components/Section03/section03";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Section03 />
       {/* <Carousel /> */}
       <HowToBuy />
       <Footer />
---
+++ src/components/Section03/section03.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./section03.css";
+
+function Section03() {
+  return (
+    <div className="section03-background">
+      <div className="bg-white bg-opacity-50 w-[855px] px-28 rounded-2xl">
+        <p className="text-[96px] Monomaniac text-white text-left">
+          fuck it we ball
+        </p>
+        <p className="text-[75px] Poetsen-font text-[#6A2B1F] text-left">
+          0/0 no tax pumpfun fair launch 5% burned on launch come join the vc
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Section03;
